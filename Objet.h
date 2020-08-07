25cb044910d41ada986b9c65bb59115adfe13a85
khaldi-aboum <59966050+khaldi-aboum@users.noreply.github.com>
2020-08-07 10:17:59 +0100

    Create Objet.h

---
+++ Objet.h
@@ -0,0 +1,22 @@
+//
+// Created by khaldi on 24‏/7‏/2020.
+//
+//#include "Node.h"
+
+#ifndef LINKEDLISTE_DEV_OBJET_H
+#define LINKEDLISTE_DEV_OBJET_H
+
+class Objet {
+public:
+    int id;
+    Objet();
+    Objet clonge();
+    bool equals(Objet );
+    short int comparisant(Objet );
+    static Objet erure();
+
+
+};
+
+
+#endif //LINKEDLISTE_DEV_OBJET_H
